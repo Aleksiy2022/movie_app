469f4fec05f59683c54fe75a4cd29612bec842bb
Aleksey Kuznetsov <kuznetcov.alekcey@icloud.com>
2025-03-26 21:56:46 +0300

    Added Input and Tabs components.

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState, Fragment } from 'react'
+import { useEffect, useState } from 'react'
+import { Input, Tabs } from 'antd'
 
 import SpinLoader from '../spin_loader/SpinLoader.jsx'
 import FilmCardsList from '../film_cards_list/FilmCardsList.jsx'
@@ -6,7 +7,7 @@ import ErrorIndicator from '../error_indicator/ErrorIndicator.jsx'
 
 export default function App({ moviesService }) {
   const [movies, setMovies] = useState([])
-  const [searchQuery, setSearchQuery] = useState('Jack+Reacher') // eslint-disable-line no-unused-vars
+  const [searchQuery, setSearchQuery] = useState('Reacher') // eslint-disable-line no-unused-vars
   const [loading, setLoading] = useState(true)
   const [errorStatus, setErrorStatus] = useState(false)
   const [errorInfo, setErrorInfo] = useState(null)
@@ -39,12 +40,45 @@ export default function App({ moviesService }) {
   const spinner = loading && isOnline ? <SpinLoader /> : null
   const content = !(loading || errorStatus || !isOnline) ? <FilmCardsList movies={movies} /> : null
   const error = errorStatus || !isOnline ? <ErrorIndicator error={errorInfo} isOnline={isOnline} /> : null
+  const searchInput = <Input placeholder="Type to search..." onChange={() => {}} />
+
+  const tabItems = [
+    {
+      key: '1',
+      label: 'Search',
+      children: searchInput,
+    },
+    {
+      key: '2',
+      label: 'Rated',
+      children: '',
+    },
+  ]
 
   return (
-    <Fragment>
+    <main
+      style={{
+        boxSizing: 'border-box',
+        margin: '0 auto',
+        padding: '15px 36px',
+        maxWidth: '1010px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: 34,
+      }}
+    >
+      <Tabs
+        defaultActiveKey="1"
+        items={tabItems}
+        centered
+        style={{
+          width: '100%',
+        }}
+      />
       {spinner}
       {content}
       {error}
-    </Fragment>
+    </main>
   )
 }
--- src/components/spin_loader/SpinLoader.jsx
+++ src/components/spin_loader/SpinLoader.jsx
@@ -3,7 +3,7 @@ import { Flex, Spin } from 'antd'
 export default function SpinLoader() {
   return (
     <Flex align="center" gap="middle">
-      <Spin fullscreen />
+      <Spin />
     </Flex>
   )
 }
